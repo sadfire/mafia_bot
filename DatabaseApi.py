792a434b18f3aa8687d9664b9c88be5852c290e1
sadbear <n9999702019@gmail.com>
2017-10-09 09:45:16 +0300

    Initial commit

---
+++ DatabaseApi.py
@@ -0,0 +1,18 @@
+import MySQLdb
+
+from GameSession import Member
+
+
+class MafiaDB:
+    def __init__(self, user, passw):
+        self.db = MySQLdb.connect(host="localhost", user=user, passwd=passw, db="mafia_rate", charset='utf8')
+        # формируем курсор, с помощью которого можно исполнять SQL-запросы
+        self.cursor = self.db.cursor()
+
+    def get_member(self, telegram_id):
+        self.cursor.execute("SELECT * FROM `Members` WHERE IdTelegram = 193019697;")
+        result = self.cursor.fetchall()
+        if len(result) == 0:
+            return None
+        tmp_member = result[0]
+        return Member(tmp_member[0], tmp_member[1], telegram_id, tmp_member[2] == 1)
