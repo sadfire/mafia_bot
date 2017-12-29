079954c1f7acd1de8ac18ed7310985e619c39732
Mikhail G Sergeevich <32374164+sadfire@users.noreply.github.com>
2017-12-29 17:56:31 +0300

    Add roles and cards

---
+++ Game/Cards.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class CardsBeginner(Enum):
+    ChangeRole = 0,
+    Bugger = 1,
+    Alibi = 2,
+    FlakJacket = 3,
+    Recruitment = 4,
+    Theft = 5,
+    Heal = 6,
+    Leader = 7,
+    Undercover = 8,
+    Neutral = 9
---
+++ Game/Roles.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Roles(Enum):
+    Mafia = 0,
+    Civilian = 1,
+    Commissar = 2
