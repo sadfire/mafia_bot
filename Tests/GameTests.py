0a8763de97b5b028f80135b754e319891899d9c7
Mikhail G Sergeevich <32374164+sadfire@users.noreply.github.com>
2017-12-30 18:55:17 +0300

    Connect unittest

--- Tests/GameTests.py
+++ Tests/GameTests.py
@@ -1,17 +1,20 @@
 from SessionHandler.Session import Session
 from Tests import Mocks
+import unittest
 
 
-def test_create_session():
-    bot = Mocks.Bot()
-    updater = Mocks.Updater()
+class MafiaTest(unittest.TestCase):
+    def test_main_scenario(self):
+        bot = Mocks.Bot()
+        updater = Mocks.Updater()
 
-    session = Session(bot, updater, 0, Mocks.Database)
-    session.query_callback(bot, Mocks.Update(""))
-    session.query_callback(bot, Mocks.Update("start_evening"))
-    for member_id in range(0, 10):
-        session.state._add_member(bot, Mocks.Update(str(member_id)))
-    session.query_callback(bot, Mocks.Update("end_evening_adding_approve"))
+        session = Session(bot, updater, 0, Mocks.Database)
+        session.query_callback(bot, Mocks.Update(""))
+        session.query_callback(bot, Mocks.Update("start_evening"))
+        for member_id in range(0, 10):
+            session.state._add_member(bot, Mocks.Update(str(member_id)))
+        session.query_callback(bot, Mocks.Update("end_evening_adding_approve"))
+
 
 if __name__ == "__main__":
-    test_create_session()
+    unittest.main()
--- Tests/Mocks.py
+++ Tests/Mocks.py
@@ -2,17 +2,17 @@ from Game.Member import Member
 
 
 class Bot:
-
     def __init__(self) -> None:
         super().__init__()
         self.index = 0
+        self._logs = []
 
     def send_message(self, chat_id, text, reply_markup=None):
-        print("bot send message with text \n {}\n".format(text))
+        self._logs.append("bot send message with text \n {}\n".format(text))
         return self
 
     def edit_reply_markup(self, reply_markup):
-        print("edit reply markup")
+        self._logs.append("edit reply markup")
         return self
 
     @property
@@ -21,30 +21,33 @@ class Bot:
         return self.index
 
     def delete_message(self, chat_id, message_id):
-        print("delete message with chat {} and id")
+        self._logs.append("delete message with chat {} and id")
+
 
 class Updater:
     def __init__(self) -> None:
         super().__init__()
         self.dispatcher = self
         self._handlers = []
+        self._logs = []
 
     def add_handler(self, handler):
         self._handlers.append(handler)
-        print("add handler {}".format(handler.__class__.__name__))
+        self._logs.append("add handler {}".format(handler.__class__.__name__))
 
     def remove_handeler(self, handler):
         if handler in self._handlers:
             self._handlers.remove(handler)
-            print("remove handler {}".format(handler.__class__.__name__))
+            self._logs.append("remove handler {}".format(handler.__class__.__name__))
         else:
-            print("can't remove handler {}".format(handler.__class__.__name__))
+            self._logs.append("can't remove handler {}".format(handler.__class__.__name__))
 
 
 class Database:
     def __init__(self, id, passw) -> None:
         super().__init__()
-        print("Database connect for {}@{}".format(id, passw))
+        self.logs = []
+        self.logs.append("Database connect for {}@{}".format(id, passw))
 
     def get_member_by_telegram(self, t_id) -> Member:
         return Member(0, "HOST", True, t_id=t_id)
@@ -52,11 +55,13 @@ class Database:
     def find_member(self, text):
         return Member(int(text), "USER_{}".format(text), True, t_id=1)
 
+
 class Update:
     def __init__(self, data='') -> None:
         super().__init__()
         self.data = data
         self.chat_id = 0
+        self._logs = []
 
     @property
     def text(self):
@@ -71,4 +76,4 @@ class Update:
         return self
 
     def edit_text(self, text):
-        print("update: edit text for {}".format(text))
+        self._logs.append("update: edit text for {}".format(text))
