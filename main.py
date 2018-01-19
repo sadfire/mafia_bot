c1c94064a485a5bfee4a271e8b8c378bf47d37e9
Mikhail G Sergeevich <n9999702019@gmail.com>
2018-01-19 18:02:20 +0300

    Revert "Revert "Token move to in parametrs""
    This reverts commit 57da09a531f0cb3feeac47a109279e00592d9c19.

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import logging
 
 from MafiaBot import Bot
+import sys
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
 def main():
-<<<<<<< HEAD
-    bot = Bot("381246435:AAF0F3U0E_KPbc8iDKCkhwuSyuKkH4bNqz4")
-=======
-    #bot = Bot("381246435:AAHOxVWGkOvk7cLsvfUl2l00Ulb6RiIxDus")#mafia_bot
-    bot = Bot("461567544:AAE2NhXbWIYDQfw2C3va4cY1-USBf9JGOx8") #MyPizdukBot
->>>>>>> 22f50a43984d8e47f229a535c7098b0df7f6b630
+    bot = Bot(sys.argv[1])
     bot.start()
 
 
 if __name__ == "__main__":
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
     main()
